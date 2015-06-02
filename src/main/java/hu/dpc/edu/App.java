fd25e600702d7c6ee5c9cb91e227be99421c76d9
Zsolt Jakab <zsolt_jakab@epam.com>
2015-06-02 16:57:27 +0200

    failonviolation

--- src/main/java/hu/dpc/edu/App.java
+++ src/main/java/hu/dpc/edu/App.java
@@ -4,18 +4,15 @@ package hu.dpc.edu;
  * Hello world!
  *
  */
-public class App 
+public class app 
 {
 	private String name;
 	
     public static void main( String[] args )
     {
-        new App().run();
+        new app().run();
     }
 	
-	class lowercaseclass {
-		
-	}
 
 	private void run() {
 		System.out.println( "Hello World! " + name.charAt(2) );
@@ -28,8 +25,7 @@ public class App
 		while(true){
 			
 		}
-		
-		if(false) return;
+
 		
 		or (int i = 0; i < 10; i++) {			// only references 'i'
 			for (int k = 0; k < 20; i++) {		// references both 'i' and 'k'
